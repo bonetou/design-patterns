3e2730828fed39d11437c3aa0cb7b8d5aea7cf7f
bonetou <h.boneto@outlook.com>
2021-03-26 10:23:30 -0300

    aula 1 - strategy pattern

---
+++ Aula 1 - Strategy/calculadora_impostos.py
@@ -0,0 +1,17 @@
+from impostos import ISS, ICMS
+
+class CalculadoraImpostos:
+
+    def realiza_calculo(self, orcamento, tipo_imposto):
+
+        imposto_calculado = tipo_imposto.calcula(orcamento)
+        print(imposto_calculado)
+
+if __name__ == '__main__':
+
+    from orcamento import Orcamento
+
+    calculadora = CalculadoraImpostos()
+    orcamento = Orcamento(500)
+    calculadora.realiza_calculo(orcamento, ISS)
+    calculadora.realiza_calculo(orcamento, ICMS)
