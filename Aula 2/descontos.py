7b66dbf4b3523d7906e5ae3bf50c0da91f5ab24d
bonetou <h.boneto@outlook.com>
2021-03-26 14:36:45 -0300

    Aula 2 - chain of responsability pattern

---
+++ Aula 2/descontos.py
@@ -0,0 +1,26 @@
+class DescontoMaisDeCincoItens:
+
+    def __init__(self, proximo_desconto):
+        self.__proximo_desconto = proximo_desconto
+
+    def calcula(self, orcamento):
+        if orcamento.total_itens > 5:
+            return orcamento.valor * 0.1
+        
+        return self.__proximo_desconto.calcula(orcamento)
+
+class DescontoMaisDeQuinhentosReais:
+
+    def __init__(self, proximo_desconto):
+        self.__proximo_desconto = proximo_desconto
+
+    def calcula(self, orcamento):
+        if orcamento.valor > 500:
+            return orcamento.valor * 0.07
+
+        return self.__proximo_desconto.calcula(orcamento)
+
+class SemDesconto:
+
+    def calcula(self, orcamento):
+        return 0
